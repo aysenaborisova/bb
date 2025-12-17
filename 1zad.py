43aff2017edc6ce4d0c6d0d6eb84f80f64a62e76
aysenaborisova <aysenaborisova159@gmail.com>
2025-12-17 10:11:48 +0900

    zad1 borisova aysena

---
+++ 1zad.py
@@ -0,0 +1,31 @@
+#2
+"""K = int(input("Введите K:"))
+N = int(input("Введите N:"))
+sum_even = 0
+for i in range(K, N +1):
+    if i % 2 == 0:
+        sum_even += i
+print("Сумма четных чисел от K до N:", sum_even)"""
+#3
+"""s = 0
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    s += n 
+print(s)"""
+#4
+"""N = int(input("Введите число N:"))
+fuct = 1
+for i in range(1,N + 1):
+    fuct *= i
+print("Факториал числа",N,"равен",fuct)"""
+#1
+"""x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if 1 < x1 < 8 or 1 < y1 < 8 or 1 < x2 < 8 or 1 < y2 < 8:
+    print((abs(x1 - x2) == 2 or abs(y1 - y2) == 1) or (abs(x1 - x2) == 1 and abs(y1 - y2) == 2))
+else:
+    print("Ошибка")"""
